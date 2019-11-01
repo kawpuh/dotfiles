c2a0480d75fe80366b3d0fd7275c3527103d26bb
ethan-sorrell <esorrell@crimson.ua.edu>
2019-11-01 18:45:13 -0500

    separate i3, autostart.sh for laptop and desktop

--- .config/i3/autostart-1monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-xset m 0/0 0 &
-xset r rate 250 50 &
-udiskie -s -2 &
-xrandr --output DVI-D-0 --off
-xinput set-prop 13 301 0, -1
---
+++ .config/i3/autostart-desktop.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+xset r rate 250 50
+xset -b
+udiskie -s -2  &
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate left 
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x288 --rotate normal
+feh --bg-fill /home/ethan/.config/10-14-Night.jpg
+redshift-gtk -t 6500K:4000K &
+nm-applet &
+gdrive_sync.sh &
+no-accel.sh &
