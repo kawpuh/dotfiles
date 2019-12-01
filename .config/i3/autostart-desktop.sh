99c94a1282d60423d74d19d6b7b314926a1f7e00
ethan-sorrell <esorrell@crimson.ua.edu>
2019-12-01 12:07:16 -0600

    update i3 config

--- .config/i3/autostart-desktop.sh
+++ .config/i3/autostart-desktop.sh
@@ -2,7 +2,7 @@
 xset r rate 250 50
 xset -b
 udiskie -s -2  &
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-0 --mode 1920x1080 --pos 0x288 --rotate normal
+xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
 feh --bg-fill /home/ethan/.config/10-14-Night.jpg
 redshift-gtk -t 6500K:4000K &
 nm-applet &
