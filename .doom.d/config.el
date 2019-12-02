781cfd47e867e164cb56f103a4082df83745c2ab
ethan-sorrell <esorrell@crimson.ua.edu>
2019-12-01 22:43:12 -0600

    update doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,11 +2,14 @@
 
 ;; Place your private configuration here
 ;;
-(setq doom-theme 'sanityinc-tomorrow-eighties
+(setq doom-theme 'sanityinc-tomorrow-night
       doom-font (font-spec :family "Go Mono" :size 16))
 
 (map! :map org-mode-map
       :nv "j" #'evil-next-visual-line
       :nv "k" #'evil-previous-visual-line)
+
+(add-hook! 'c++-mode-hook (lambda ()
+                            (push '(?< . ("<" . ">")) evil-surround-pairs-alist)))
 
 ;; (add-hook! org-mode (setq truncate-lines nil))
