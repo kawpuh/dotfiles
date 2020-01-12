979e5b088852125080c54de2e63e13e2384b0379
ethan <esorrell@crimson.ua.edu>
2020-01-12 05:15:40 -0600

    add autojump and em

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+# set autojump
+begin
+    set --local AUTOJUMP_PATH /usr/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
+
+alias em="emacs -nw"
