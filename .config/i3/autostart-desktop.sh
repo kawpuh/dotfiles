a7bfb92117ae1116d9b73a52826177082f3c546a
ethan-sorrell <esorrell@crimson.ua.edu>
2020-03-28 11:27:39 -0500

    remove gdrive sync from autostart

--- .config/i3/autostart-desktop.sh
+++ .config/i3/autostart-desktop.sh
@@ -6,5 +6,4 @@ xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output H
 feh --bg-fill /home/ethan/.config/10-14-Night.jpg
 redshift-gtk -t 6500K:4000K &
 nm-applet &
-gdrive_sync.sh &
 no-accel.sh &
