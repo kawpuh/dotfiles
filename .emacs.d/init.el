b82d0ccfb0004b432d08a5fdd7bb83d90b2023f0
ethan-sorrell <esorrell@crimson.ua.edu>
2020-04-12 21:41:40 -0500

    temp emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,11 @@
+;; disable ui elements
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq inhibit-splash-screen t)
+
+(server-start)
+
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
@@ -20,6 +28,7 @@
   (setq evil-want-keybinding nil) ;; required for evil-collection
   :config
   (evil-mode)
+  (evil-set-leader 'normal " ")
   (use-package evil-collection
     :after evil
     :ensure t
@@ -46,8 +55,39 @@
     (evil-define-key 'operator global-map "S" 'evil-Surround-edit)
     (evil-define-key 'visual global-map "S" 'evil-surround-region)
     (evil-define-key 'visual global-map "s" 'evil-surround-region)
-    (evil-define-key 'visual global-map "gS" 'evil-Surround-region)))
-					;
+    (evil-define-key 'visual global-map "gS" 'evil-Surround-region))
+
+  (use-package evil-lion
+    :ensure t
+    :config
+    (evil-lion-mode))
+
+  ;; other evil config
+  (evil-define-key 'normal global-map (kbd "<leader>fc") 
+		   (lambda () (interactive) (find-file "~/.emacs.d/init.el"))))
+
+;; treemacs
+(use-package treemacs
+  :after evil
+  :ensure t
+  :defer t
+  :commands
+  (treemacs)
+  :init
+  (evil-define-key 'normal global-map (kbd "<leader>ft") 'treemacs))
+
+(use-package cider
+  :ensure t
+  :after evil
+  :defer t
+  :commands
+  (cider-jack-in
+   cider-eval-region
+   cider-eval-buffer)
+  :init
+  (evil-define-key 'normal global-map (kbd "<leader>rr") 'cider-jack-in)
+  (evil-define-key 'visual global-map (kbd "<leader>er") 'cider-eval-region))
+
 ;; load custom-set-variables
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
