ca0afb88aa80d6289094e3d51c4921cca4733d81
ethan-sorrell <ethan@debian>
2021-03-24 16:50:18 -0500

    cleaning up bin

--- bin/gdrive_sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rclone copy crimsondrive:Calibre\ Library ~/Calibre\ Library;
-while true; do\
-          sleep 2s; \
-          find ~/Calibre\ Library/ | entr -d rclone copy ~/Calibre\ Library/ crimsondrive:Calibre\ Library;\
-          done
