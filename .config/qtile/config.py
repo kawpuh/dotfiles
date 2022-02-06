65b57b7605b3b68653c4665bd2fc33ac811a0526
kawpuh <e.r.sorrell@gmail.com>
2022-02-06 13:24:54 -0600

    disable cursor warp

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -261,7 +261,7 @@ dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
-cursor_warp = True
+cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
