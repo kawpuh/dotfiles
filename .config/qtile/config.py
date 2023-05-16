a77d7b3c6ba9613000b56060c03914f3bac8afd2
kawpuh <e.r.sorrell@gmail.com>
2023-05-16 17:17:28 -0500

    improved mouse keys and experimenting w/ bg_music

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -150,6 +150,10 @@ keys = [
     Key([MOD], "Down", lazy.spawn("xdotool mousemove_relative 0 15")),
     Key([MOD], "Left", lazy.spawn("xdotool mousemove_relative -- -15 0")),
     Key([MOD], "Right", lazy.spawn("xdotool mousemove_relative 15 0")),
+    Key([MOD, "Shift"], "Up", lazy.spawn("xdotool mousemove_relative -- 0 -300")),
+    Key([MOD, "Shift"], "Down", lazy.spawn("xdotool mousemove_relative 0 300")),
+    Key([MOD, "Shift"], "Left", lazy.spawn("xdotool mousemove_relative -- -300 0")),
+    Key([MOD, "Shift"], "Right", lazy.spawn("xdotool mousemove_relative 300 0")),
     KeyChord([MOD], "Next", [
         Key([], "Left", lazy.spawn("xdotool click 1")),
         Key([], "Right", lazy.spawn("xdotool click 3")),
