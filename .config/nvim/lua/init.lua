07c40814faed785ad3119836a0dabe7866e5fb5f
kawpuh <e.r.sorrell@gmail.com>
2023-08-29 12:05:03 -0500

    more nvim completion config (cmp match lcs)

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -10,7 +10,7 @@ cmp.setup({
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-l>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-l>'] = cmp.mapping.complete_common_string()
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
