189c1adaa0805fefd06e3b0c51e9dd64524ca0cc
kawpuh <e.r.sorrell@gmail.com>
2025-07-24 16:27:42 -0500

    qutebrowser show chrome user agent

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -67,7 +67,7 @@ c.aliases = {"q": "close",
 c.content.javascript.log_message.excludes = \
 {"userscript:_qute_stylesheet": ["*Refused to apply inline style because it violates the following Content Security Policy directive: *"],
  "userscript:_qute_js": ["Uncaught TypeError: Cannot read properties of undefined (reading 'length')"]}
-
 c.hints.chars = "asdfghjklqwertyuiopzxcvbnm"
+c.content.headers.user_agent = "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) Chrome/{upstream_browser_version_short} Safari/{webkit_version}"
 
 config.load_autoconfig(False)
